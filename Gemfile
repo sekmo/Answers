c1e745252470e81520427fe64bdbeb87fd8dc50b
sekmo <francesco.mari@daliaresearch.com>
2018-10-12 18:41:56 +0200

    wip banging head

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.3.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 gem 'devise'
+gem 'bootstrap', '~> 4.1.3'
+gem 'jquery-rails'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,15 @@ module ApplicationHelper
     render(partial: "shared/error_messages", locals: {object: object})
   end
 
+  def flash_class(level)
+    case level
+      when "success" then "alert alert-success"
+      when "alert" then "alert alert-warning"
+      when "error" then "alert alert-danger"
+      else "alert alert-info"
+    end
+  end
+
   def nice_local_time(time)
     time.localtime.strftime('%d/%m/%Y %H:%m')
   end
